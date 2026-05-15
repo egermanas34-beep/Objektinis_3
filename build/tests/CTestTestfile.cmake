5ad423f17c1f38dbaf7db614bcce696c7dcc216d
egermanas34-beep <egermanas34@gmail.com>
2026-05-15 11:47:37 +0300

    Delete build directory

--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/objektinis/v3.0/tests
-# Build directory: D:/objektinis/v3.0/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/objektinis/v3.0/build/tests/VectorTest[1]_include.cmake")
